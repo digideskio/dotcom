4f7f5b4737669aadb9439d37135634f75d11b2db
Olivia <olivia@praytellagency.com>
2016-05-12 16:32:26 -0400

    New & Updated Staff

---
+++ team/alexa.php
@@ -0,0 +1,157 @@
+<?php
+$title="Alexa Speed";
+$description="Praytell is a Brooklyn-based creative communications agency blending traditional PR with new school social and digital. An experienced team with media, design, and tech chops ready to shake things up.";
+$breadcrumb="Who We Are";
+ ?>
+
+
+<?php 
+   include($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"); 
+?>   
+
+			<section class="image-block short-pad team-member">
+		
+				<div class="image-holder col-md-4 col-sm-4 pull-left">
+					<div class="background-image-holder">
+						<img alt="<?php echo $title ?> // Praytell" title="<?php echo $title ?> // Praytell" class="background-image" src="/img/team/adam-hover.jpg">
+					</div>
+				</div>
+		
+				<div class="container">
+					<div class="row">
+                    
+						<div class="col-md-8 col-sm-8 col-md-offset-5 col-sm-offset-5">
+                        <div class="row">
+								<div class="team-member-title pad-bottom">
+                                    <div class="detail-line"></div>
+                                    <h5>Analyst</h5>
+                                     <h1 class="text"><span id="typed"></span></h1>    
+								</div>
+                    	</div>
+                        
+
+							<div class="col-md-6 col-sm-6 col-xs-6">
+								<div class="narrow-feature">
+									<i class="icon pe-7s-car"></i>
+									<div class="content">
+										<h1>8,650<sub>episodes</sub></h1>
+										<p>of podcasts listened to since 2011 (that's 1,501 hours or 2 months)</p>
+									</div>
+								</div>
+							</div>
+						
+							<div class="col-md-6 col-sm-6 col-xs-6">
+								<div class="narrow-feature">
+									<i class="icon ion-ios-speedometer-outline"></i>
+									<div class="content">
+										<h1>2</h1>
+										<p>cats owned</p>
+									</div>
+								</div>
+							</div>
+						
+							<div class="col-md-6 col-sm-6 col-xs-6">
+								<div class="narrow-feature">
+									<i class="icon ion-ios-navigate-outline"></i>
+									<div class="content">
+										<h1>2:1</h1>
+										<p>ratio of cows to people from my home county in upstate NY</p>
+									</div>
+								</div>
+							</div>
+						
+							<div class="col-md-6 col-sm-6 col-xs-6">
+								<div class="narrow-feature">
+									<i class="icon ion-ios-lightbulb-outline"></i>
+									<div class="content">
+										<h1>3</h1>
+										<p>adult coloring books owned</p>
+									</div>
+								</div>
+							</div>
+                                                        
+						</div>
+					
+					</div>
+                    
+                    
+				</div>
+			
+			</section>
+            
+			<section class="action-strip-2">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<a href="/who-we-are#praytell-crew" class="btn inner-link"  alt="Back To The Praytell Crew" title="Back To The Praytell Crew"><i class="ion-arrow-left-a"></i> To The Praytell Crew</a>
+						</div>
+					</div>
+				</div>
+			</section>     
+            
+                   
+			<!--<<section class="twitter-1">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-7 col-sm-9">
+							<i class="icon ion-social-twitter-outline text-white"></i>
+							<div class="twitter-feed">
+								<div class="tweets-feed tweets-slider" data-widget-id="628321945428316164">
+								</div>
+							</div>
+							<span class="text-white">Follow <a href="https://www.twitter.com/ADZNstudio">@ADZNstudio</a></span>
+						</div>
+					</div>
+				</div>
+			</section>-->
+            
+			<section class="instagram-1">
+				<div class="instafeed" data-user-name="speedtastic">
+					<ul></ul>
+				</div>
+			
+				<div class="container vertical-align">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<a href="#">
+								<h2 class="text-white"><i class="icon social_instagram"></i>@speedtastic</h2><br>
+							</a>
+						</div>
+					</div>
+				</div>
+			</section>
+	
+    
+    <?php 
+   include($_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"); 
+?> 
+
+<script>
+        $(function(){
+    
+            $("#typed").typed({
+                strings: ["Alexa Speed^6000", "Goldeny"],
+                typeSpeed: 10,
+                backDelay: 2000,
+            	backSpeed: -30,
+                loop: true,
+                contentType: 'html', // or text
+                // defaults to false for infinite loop
+                loopCount: false,
+                callback: function(){ foo(); },
+                resetCallback: function() { newTyped(); }
+            });
+    
+            $(".reset").click(function(){
+                $("#typed").typed('reset');
+            });
+    
+        });
+    
+        function newTyped(){ /* A new typed object */ }
+    
+        function foo(){ console.log("Callback"); }
+    
+        </script>
+
+				
