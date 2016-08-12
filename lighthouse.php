b1cea2acc3257e10931d571cde160b55d57c7787
Olivia <olivia@praytellagency.com>
2016-08-12 16:58:08 -0400

    Lighthouse page
    Lighthouse facelift

--- lighthouse.php
+++ lighthouse.php
@@ -9,31 +9,27 @@ $breadcrumb="Who We Are";
    include($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"); 
 ?> 
         				
-			<section class="double-pad">
-				<div class="background-image-holder  overlay-light-gradient">
-					<img alt="Slide Background" class="background-image" src="img/lighthouse.jpg">
-				</div>	
-			
-				<div class="container">
-						<div class="row">
-							<div class="col-md-5 col-sm-7">
-									<h1 class="white-space:pre;"><span id="typed"></span></h1>
-								<p class="lead">
-									There's a painting of a lighthouse in our office, given to us by Larry Pray, Andy's dad, early on in our journey together. When he sent it, he offered these words: 
-								</p>
-							</div>
-						</div>
-				</div>
-			
-			</section>
 
-            <section class="large-pad">
+
+            <section style="background-color:#0054a6; color:#ffffff; padding:120px 0px 50px 0px;">
 		
+				<div class="container">
+					<div class="row" >
+						<div class="col-md-6 col-md-offset-3 col-sm-12 text-center">
+                                <img src="img/lighthouse-500x500.png" style="width:400px; height:auto;">
+                
+                                <h1 style="color:#ffffff">Lighthouse</h1>
+								<h4 style="color:#ffffff">Larry Pray, Andy's dad, gave us this painting of a lighthouse early on in our journey together.<br />With it, he offered these words: </h4>
+					</div>
+				</div>
+			</div>
+			</section>
+         <section class="medium-pad">
 				<div class="container">
 					<div class="row">
 						<div class="col-md-8 col-md-offset-2 col-sm-12 text-center">
-                            <p class="super-lead move-up">
-									<em>"The idea is that when companies are wondering what to do, they need to head for the lighthouse. Such a lighthouse is off the shore of Popham Beach in Maine, where you first touched water. I wanted Praytell to have a painting that showed an in-motion ocean banging against something solid, with the light of a new day in the distance. This is what Praytell is all about. Lost? Give us a call. Caught in a storm? Give us a call. We're here to help, we're in this together, you can count on us."</em>
+                            <p class="super-lead">
+									<em>When companies are wondering what to do, they need to head for the lighthouse. Such a lighthouse is off the shore of Popham Beach in Maine, where you first touched water. I wanted Praytell to have a painting that showed an in-motion ocean banging against something solid, with the light of a new day in the distance. This is what Praytell is all about. Lost? Give us a call. Caught in a storm? Give us a call. We're here to help, we're in this together, you can count on us.</em>
                             </p>
 					</div>
 				</div>
@@ -41,37 +37,30 @@ $breadcrumb="Who We Are";
 			</section>
 
 
-			<section class="image-block">
+			<section style="background-color:#ffffff; padding:0px">
+				
+					<div class="row" style="padding:0px; margin:0px">
+						<div class="col-md-6 col-sm-12 col-md-offset-3">
+                            <img src="img/larrypray-painting2.jpg" class="img-responsive center-block">
+                        </div>
+					</div>
+			
+
+			</section>
+              <section class="medium-pad">
 				<div class="container">
 					<div class="row">
-						<div class="col-md-5 col-sm-7 col-md-offset-7 col-sm-offset-5">
-							<div class="text-block">
-								<div class="detail-line"></div>
-								
-								<p class="">
-									Since that day, back in a tiny shared office space in DUMBO, the painting has served as a gentle reminder to us all. To put service first. To stand the test of time. Be the first to shine in the dark, the first to greet folks to shore. A light that stands for something strong and true.</p> 
+						<div class="col-md-8 col-md-offset-2 col-sm-12 text-center">
+                          <p class="super-lead">Since that day, back in a tiny shared office space in DUMBO, the painting has served as a gentle reminder to us all. To put service first. To stand the test of time. Be the first to shine in the dark, the first to greet folks to shore. A light that stands for something strong and true.</p>
 
-<p>Here's to the gentle daily reminders that matter, especially amidst crashing waves. </p>
-								</p>
-							</div>
-						</div>
+                           <p class="super-lead">Here's to the gentle daily reminders that matter, especially amidst crashing waves.</p>
+                            
 					</div>
 				</div>
-			
-				<div class="image-holder col-md-6 col-sm-4 pull-left">
-					<div class="image-slider">
-						<ul class="slides">
-							<li>
-								<div class="background-image-holder">
-									<img alt="Slide Background" class="background-image" src="img/larrypray-painting2.jpg">
-								</div>
-							</li>
-							
-						</ul>
-					</div>
-				</div>
-			
+			</div>
 			</section>
+
+
 			<section class="action-strip-2">
 				<div class="container">
 					<div class="row">
---
+++ team/alexb.php
@@ -0,0 +1,144 @@
+<?php
+$title="Alex Blynn";
+$description="Praytell is a Brooklyn-based creative communications agency blending traditional PR with new school social and digital. An experienced team with media, design, and tech chops ready to shake things up.";
+$breadcrumb="Who We Are";
+ ?>
+
+
+<?php 
+   include($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"); 
+?>   
+
+			<section class="image-block short-pad team-member">
+		
+				<div class="image-holder col-md-4 col-sm-4 pull-left">
+					<div class="background-image-holder">
+						<img alt="<?php echo $title ?> // Praytell" title="<?php echo $title ?> // Praytell" class="background-image" src="/img/team/alex-hover.jpg">
+					</div>
+				</div>
+		
+				<div class="container">
+					<div class="row">
+                    
+						<div class="col-md-8 col-sm-8 col-md-offset-5 col-sm-offset-5">
+                        <div class="row">
+								<div class="team-member-title pad-bottom">
+                                    <div class="detail-line"></div>
+                                    <h5>PR Strategist</h5>
+                                     <h1 class="text"><span id="typed"></span></h1>    
+								</div>
+                    	</div>
+                            
+                        
+
+							<div class="col-md-6 col-sm-6 col-xs-6">
+								<div class="narrow-feature">
+									<i class="icon ion-ios-personadd-outline"></i>
+									<div class="content">
+										<h1>7</h1>
+										<p>nieces and nephews I have (possibly more in the near future)</p>
+									</div>
+								</div>
+							</div>
+						
+							<div class="col-md-6 col-sm-6 col-xs-6">
+								<div class="narrow-feature">
+									<i class="icon ion-jet"></i>
+									<div class="content">
+										<h1>1</h1>
+										<p>Siamese cat by the name of Sterling who probably calls me his servant</p>
+                                        <p>&nbsp;</p>
+									</div>
+								</div>
+							</div>
+						
+							<div class="col-md-6 col-sm-6 col-xs-6">
+								<div class="narrow-feature">
+									<i class="icon pe-7s-world"></i>
+									<div class="content">
+										<h1>4</h1>
+										<p>lint rollers I keep on hand for all the cat hair</p>
+									</div>
+								</div>
+							</div>
+						
+							<div class="col-md-6 col-sm-6 col-xs-6">
+								<div class="narrow-feature">
+									<i class="icon pe-7s-star"></i>
+									<div class="content">
+										<h1>90</h1>
+										<p>percentage of my clothing that is black</p>
+                                        <p>&nbsp;</p>
+									</div>
+								</div>
+							</div>
+                                                        
+						</div>
+					
+					</div>
+                    
+                    
+				</div>
+			
+			</section>
+            
+			<section class="action-strip-2">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<a href="/who-we-are#praytell-crew" class="btn inner-link"  alt="Back To The Praytell Crew" title="Back To The Praytell Crew"><i class="ion-arrow-left-a"></i> To The Praytell Crew</a>
+						</div>
+					</div>
+				</div>
+			</section>     
+            
+                   
+			<section class="twitter-1">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-7 col-sm-9">
+							<i class="icon ion-social-twitter-outline text-white"></i>
+							<div class="twitter-feed">
+								<div class="tweets-feed tweets-slider" data-widget-id="730861667479896064">
+								</div>
+							</div>
+							<span class="text-white">Follow <a href="https://www.twitter.com/AlexBlynn">@AlexBlynn</a></span>
+						</div>
+					</div>
+				</div>
+			</section>
+            
+    
+    <?php 
+   include($_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"); 
+?> 
+
+<script>
+        $(function(){
+    
+            $("#typed").typed({
+                strings: ["Alex Howe^6000", "Alexis Bledel"],
+                typeSpeed: 10,
+                backDelay: 2000,
+            	backSpeed: -30,
+                loop: true,
+                contentType: 'html', // or text
+                // defaults to false for infinite loop
+                loopCount: false,
+                callback: function(){ foo(); },
+                resetCallback: function() { newTyped(); }
+            });
+    
+            $(".reset").click(function(){
+                $("#typed").typed('reset');
+            });
+    
+        });
+    
+        function newTyped(){ /* A new typed object */ }
+    
+        function foo(){ console.log("Callback"); }
+    
+        </script>
+
+				
